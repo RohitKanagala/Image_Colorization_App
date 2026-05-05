f52f347e9d713b60c87eed17ef5c76d8fe2358ab
Kanagala Rohit Lakshmi Narasimha Naidu <rohitkanagala18@gmail.com>
2026-05-05 20:55:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ from PIL import Image
 import io
 from colorize import colorize
 
-st.set_page_config(page_title="AI Image Colorization", layout="centered")
-
+st.set_page_config(page_title="Image Colorization", layout="centered")
 st.title("Black & White Image Colorization")
 st.write("Drag & Drop or Browse a Black & White Image to Colorize")
 
@@ -15,40 +14,43 @@ uploaded = st.file_uploader(
     type=["jpg", "png", "jpeg"],
     help="Drag and drop or click to upload an image"
 )
-
 if uploaded is not None:
-
     image = Image.open(uploaded).convert("RGB")
-
+    image_np = np.array(image)
+    generate = st.button("Colorize Image")
     col1, col2 = st.columns(2)
 
     with col1:
         st.subheader("Original Image")
         st.image(image, width="stretch")
 
-    image_np = np.array(image)
+    with col2:
+        st.subheader("Colorized Image")
+        result_placeholder = st.empty()
+        download_placeholder = st.empty()
 
-    if st.button("Colorize Image"):
+    if generate:
 
-        image_np = cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR)
+        with st.spinner("Colorizing image..."):
 
-        result = colorize(image_np)
+            img_bgr = cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR)
 
-        result = cv2.cvtColor((result * 255).astype("uint8"),
-                              cv2.COLOR_BGR2RGB)
+            result = colorize(img_bgr)
 
-        with col2:
-            st.subheader("Colorized Image")
-            st.image(result, width="stretch")
-
-            result_pil = Image.fromarray(result)
-            buf = io.BytesIO()
-            result_pil.save(buf, format="PNG")
-            byte_im = buf.getvalue()
-
-            st.download_button(
-                label="Download Colorized Image",
-                data=byte_im,
-                file_name="colorized_image.png",
-                mime="image/png"
+            result = cv2.cvtColor(
+                (result * 255).astype("uint8"),
+                cv2.COLOR_BGR2RGB
             )
+
+        result_placeholder.image(result, width="stretch")
+
+        result_pil = Image.fromarray(result)
+        buf = io.BytesIO()
+        result_pil.save(buf, format="PNG")
+
+        download_placeholder.download_button(
+            label="Download Colorized Image",
+            data=buf.getvalue(),
+            file_name="colorized_image.png",
+            mime="image/png"
+        )
